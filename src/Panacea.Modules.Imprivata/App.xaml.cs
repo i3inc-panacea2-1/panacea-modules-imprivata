fc0d6d75e7e683d63e016f03986d46ffcf9e1420
Portno <j.remoundos@dotbydot.gr>
2019-06-24 13:04:03 +0300

    nuget + text size

---
+++ src/Panacea.Modules.Imprivata/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Panacea.Modules.Imprivata
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        public App()
+        {
+
+        }
+    }
+}
+
